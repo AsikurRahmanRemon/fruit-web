d552c85118007504b1d0a39812c5a6c7d6562888
AsikurRahmanRemon <asikremon17@gmail.com>
2023-06-12 11:40:07 +0600

    added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let searchBtn = document.querySelector('#search-btn');
 let searchBar = document.querySelector('.search-bar-container');
 
-window.onscroll = () =>{
+window.addEventListener('scroll', () => {
   searchBtn.classList.remove('fa-times');
   searchBar.classList.remove('active');
 });
